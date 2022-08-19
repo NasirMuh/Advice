a949ff1d5ae9cdc6a05fa4357409d64311686b79
NasirMuh <octobernine1995@gmail.com>
2022-08-20 01:05:20 +0500

    adding tailwind css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
+// module.exports = {
+//   reactStrictMode: true,
+// }
--- pages/advice.js
+++ pages/advice.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react'
 const Advice = () => {
-
   const [advice, setAdvice] = useState('')
 
   const getAdvice = async () => {
@@ -9,27 +7,21 @@ const Advice = () => {
     const response = await fetch(url)
     const responseJson = await response.json()
     setAdvice(responseJson.slip.advice)
-    console.log(responseJson.slip.advice)
   }
-  // useEffect(() => {
-  
-    // getAdvice()
-
-    // axios.get("https://api.adviceslip.com/advice")
-    //   .then((response) => {
-    //     const { advice1 } = response.data.slip
-    //     // console.log(advice)
-    //     setAdvice(advice1)
-    //   })
-    //   .catch((error) => {
-    //     console.log(error)
-    //   })
-  // })
 
   return (
     <>
-      <h1>advice : {advice}</h1>
-      <button onClick={() => getAdvice()}> Get Advice</button>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col border-8 bg-gray-100 border-gray-200 rounded-lg mt-10">
+          <div className="flex justify-center mb-4">
+            <button onClick={() => getAdvice()} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Advice</button>
+          </div>
+          <div className="text-center lg:w-2/3 w-full   lg:h-36 md:h-56">
+            <p className=" leading-relaxed justify-center items-center p-8">{advice}</p>
+          </div>
+        </div>
+      </section>
+
     </>
   )
 }

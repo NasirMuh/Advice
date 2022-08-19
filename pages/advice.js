056139b71344df8420c3ef6a115b24f1256958ff
NasirMuh <octobernine1995@gmail.com>
2022-08-19 23:38:40 +0500

    working with apis

--- pages/advice.js
+++ pages/advice.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Advice = () => {
+
+  const [advice, setAdvice] = useState('')
+
+  const getAdvice = async () => {
+    const url = "https://api.adviceslip.com/advice"
+    const response = await fetch(url)
+    const responseJson = await response.json()
+    setAdvice(responseJson.slip.advice)
+    console.log(responseJson.slip.advice)
+  }
+  // useEffect(() => {
+  
+    // getAdvice()
+
+    // axios.get("https://api.adviceslip.com/advice")
+    //   .then((response) => {
+    //     const { advice1 } = response.data.slip
+    //     // console.log(advice)
+    //     setAdvice(advice1)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+  // })
+
   return (
     <>
-        <h1>Advice data</h1>
+      <h1>advice : {advice}</h1>
+      <button onClick={() => getAdvice()}> Get Advice</button>
     </>
   )
 }
